2348a028e297cae3973361f0b574a5b08b59134e
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-07-18 22:43:23 -0400

    Drop work on Fenrir.
    It's a bit more work than I want to put in right now.

--- guix/nongnu/packages/accessibility.scm
+++ guix/nongnu/packages/accessibility.scm
@@ -1,33 +1,32 @@
 (define-module (nongnu packages accessibility)
   #:use-module (guix packages)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages audio)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages speech)
-  #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages admin)
-  #:use-module (gnu packages terminals)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix build-system python)
+  #:use-module (guix build-system meson)
   #:use-module (guix licenses))
-(define-public fenrir
+(define-public espeakup
   (package
-   (name "fenrir")
-   (version "1.9.7")
-   (source (origin
-            (method git-fetch)
-            (uri (git-reference
-                  (url "https://github.com/chrys87/fenrir")
-                  (commit version)))
-            (file-name (git-file-name name version))
-            (sha256
-             (base32
-              "031w94h1wyanhmlrn567ckdvdxy1axi7c7bafmna2yjvdl45kzda"))))
-   (build-system python-build-system)
-   (inputs (list sox espeak-ng python-daemonize python-pyudev python-dbus python-pyte python-pyudev))
-   (license lgpl3+)
-   (description
-    "Fenrir is a moder TTY screenreader written in Python 3. It supports espeak, brltty for Braille output, and sound icons")
-   (home-page "https://linux-a11y.org/index.php?page=fenrir-screenreader")
-   (synopsis "A modern TTY screenreader for Linux")))
-fenrir
+    (name "espeakup")
+    (version "0.90")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/linux-speakup/espeakup")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+                            (sha256
+               (base32
+                "0lmjwafvfxy07zn18v3dzjwwpnid2xffgvy2dzlwkbns8gb60ds2"))))
+    (build-system meson-build-system)
+    (native-inputs (list pkg-config))
+    (inputs (list espeak-ng alsa-lib))
+    (license gpl3+)
+    (synopsis "A bridge for espeak and speakup")
+    (description
+     "Ditto")
+    (home-page "https://github.com/williamh/espeakup")))
