3bf072cc00a1f13687d93fadebb3b1915b815a7a
Hunter Jozwiak <hunter.t.joz@gmail.com>
2022-07-18 22:59:11 -0400

    Inital iso file.

---
+++ guix/installer.scm
@@ -0,0 +1,5 @@
+(define-module (nongnu system installer)
+  #:use-module (gnu services)
+  #:use-module (gnu system)
+  #:use-module (gnu system install)
+  #:use-module (nongnu packages linux accessibility))
